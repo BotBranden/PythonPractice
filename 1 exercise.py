f8b57d5fcae3d4aed26847af32fe465c9fd518a3
Anton <test@example.com>
2023-03-08 22:35:56 +0200

    Added the line

--- 1 exercise.py
+++ 1 exercise.py
@@ -6,3 +6,5 @@ print (f"U are {Age}, so in {Year100} u will be 100 year old")
 Num = input("Fine, now put any number from 1-10: ")
 Num = int(Num)
 print (f"This message is repated {Num} times! :D\n"*Num)
+
+#anton test
